f6092317832de8700244fbbc69c81624347904ab
Faidhon Nur <faidhon.nur@samsung.com>
2021-06-25 14:10:20 +0700

    Add toast for now

--- app/src/main/java/com/samsung/android/plugin/inputdevicereader/MainActivity.kt
+++ app/src/main/java/com/samsung/android/plugin/inputdevicereader/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.InputDevice
 import android.view.KeyEvent
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.samsung.android.plugin.inputdevicereader.adapter.InputDeviceListAdapter
@@ -13,15 +14,15 @@ import com.samsung.android.plugin.inputdevicereader.data.InputDeviceItem
 
 class MainActivity : AppCompatActivity(), InputDeviceListAdapter.OnItemClickListener {
 
-    private var selectedInputDeviceID = 0;
-    private var list = ArrayList<InputDeviceItem>();
+    private var selectedInputDeviceID = 0
+    private var list = ArrayList<InputDeviceItem>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         for(id in InputDevice.getDeviceIds()) {
-            val inputDevice : InputDevice = InputDevice.getDevice(id);
-            val item = InputDeviceItem(id.toString(), inputDevice.name, inputDevice)
+            val inputDevice : InputDevice = InputDevice.getDevice(id)
+            val item = InputDeviceItem(inputDevice.name, inputDevice.descriptor, inputDevice)
             list += item
 
         }
@@ -36,8 +37,10 @@ class MainActivity : AppCompatActivity(), InputDeviceListAdapter.OnItemClickList
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         val inputDevice = event?.device
         if(inputDevice?.id == selectedInputDeviceID) {
-            Log.d("VELL_EVENT", keyCode.toString() + " from " + inputDevice?.name)
+            Log.d("VELL_EVENT", keyCode.toString() + " from " + inputDevice.name)
 
+            val toast = Toast.makeText(applicationContext, "received " + keyCode + " from " + inputDevice.name, Toast.LENGTH_SHORT)
+            toast.show()
             return true
             //if keyCode is more than a byte then this callback will be called twice
             //return true prevents it
@@ -48,7 +51,10 @@ class MainActivity : AppCompatActivity(), InputDeviceListAdapter.OnItemClickList
 
     override fun onItemClicked(position: Int) {
 
-        selectedInputDeviceID = list.get(position).text1.toInt()
+        selectedInputDeviceID = list.get(position).data.id
+        val toast = Toast.makeText(applicationContext, "listening data from " + list.get(position).data.name, Toast.LENGTH_SHORT)
+        toast.show()
+
         Log.d("VELL_UI", "user click " + position)
     }
 }
